4f78ade3445fd154c60fc62b820cc29effd81f67
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-10-20 01:00:36 +0200

    Update rtmp.py

--- rtmp.py
+++ rtmp.py
@@ -1,5 +1,4 @@
 import subprocess
-import ffpmeg
 
 # Commande ffmpeg pour envoyer une vidéo noire sans son
 command = [
@@ -7,7 +6,7 @@ command = [
     '-f', 'lavfi',            # Utilisation du filtre lavfi
     '-i', 'color=c=black:s=1280x720:d=3600',  # Crée une vidéo noire de résolution 1280x720 pendant 1 heure (3600 secondes)
     '-f', 'flv',              # Format de sortie en FLV pour RTMP
-    'rtmp://your-rtmp-server/live/stream-key'  # Remplacer par votre URL RTMP
+    'rtmp://your-rtmp-server/live/53qa-y81q-px7q-8g6y-78zb'  # Remplacer par votre URL RTMP
 ]
 
 # Exécution de la commande
