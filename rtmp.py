7b6d4f53a8d8935b83f61cbc38913831f7ad58c8
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-10-20 01:00:59 +0200

    Update rtmp.py

--- rtmp.py
+++ rtmp.py
@@ -6,7 +6,7 @@ command = [
     '-f', 'lavfi',            # Utilisation du filtre lavfi
     '-i', 'color=c=black:s=1280x720:d=3600',  # Crée une vidéo noire de résolution 1280x720 pendant 1 heure (3600 secondes)
     '-f', 'flv',              # Format de sortie en FLV pour RTMP
-    'rtmp://your-rtmp-server/live/53qa-y81q-px7q-8g6y-78zb'  # Remplacer par votre URL RTMP
+    'rtmp://a.rtmp.youtube.com/live2/53qa-y81q-px7q-8g6y-78zb'  # Remplacer par votre URL RTMP
 ]
 
 # Exécution de la commande
