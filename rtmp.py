46237f2f81bc09e6d38525bed4fa08698bd9f6f6
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-10-20 11:06:25 +0200

    Update rtmp.py

--- rtmp.py
+++ rtmp.py
@@ -5,7 +5,7 @@ command = [
     'ffmpeg',
     '-re',                      # Lire à la vitesse réelle
     '-stream_loop', '-1',        # Répéter la vidéo en boucle infinie
-    '-i', 'lv_0_20241020025552.mp4',  # Vidéo source
+    '-i', 'lv_0_20241020110446.mp4',  # Vidéo source
     '-c:v', 'libx264',           # Codeur vidéo H.264
     '-preset', 'veryfast',       # Réduction de la latence
     '-b:v', '3000k',             # Débit vidéo de 3000 kbps
