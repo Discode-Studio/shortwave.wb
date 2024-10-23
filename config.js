7fd4e4c6eb3dcce9e3cc3c44b70db32a476b7aa6
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-10-23 17:14:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 // If you use this please specify multiple kiwis so you don't just camp on a single one!
-// URL format: http://kiwi.com:8073/?keys=x - timeout - 24h usage limit (time limits in minutes, null means no limit)
+// URL format: http://kiwi.com:8073/ - timeout - 24h usage limit (time limits in minutes, null means no limit)
 const kiwilistDay = [
-    { url: "http://okno.ddns.net:8073/?keys=x", timeout: 24, timelimit: null },
-    { url: "http://188.162.63.103:8073/?keys=x", timeout: 30, timelimit: 120 },
-    { url: "http://37.33.50.162:8073/?keys=x", timeout: 30, timelimit: 120 }
+    { url: "http://okno.ddns.net:8073/", timeout: 24, timelimit: null },
+    { url: "http://188.162.63.103:8073/", timeout: 30, timelimit: 120 },
+    { url: "http://37.33.50.162:8073/", timeout: 30, timelimit: 120 }
 ];
 
 const kiwilistNight = [
-    { url: "http://kiwi-kuo.aprs.fi:8073/?keys=x", timeout: 30, timelimit: 120 },
-    { url: "http://188.162.63.103:8073/?keys=x", timeout: 30, timelimit: 120 },
-    { url: "http://warszawa.proxy.kiwisdr.com:8073/?keys=x", timeout: 120, timelimit: 120 },
+    { url: "http://kiwi-kuo.aprs.fi:8073/", timeout: 30, timelimit: 120 },
+    { url: "http://188.162.63.103:8073/", timeout: 30, timelimit: 120 },
+    { url: "http://warszawa.proxy.kiwisdr.com:8073/", timeout: 120, timelimit: 120 },
 ];
 
 const reloadMinutes = 25; // max time until next reload after finding a receiver
@@ -22,7 +22,7 @@ const kiwiConfig = {
     startPass: "0",
     endPass: "4000",
     colormap: "4",
-    volume: "180",
+    volume: "180&keys=x",
 };
 
 const config = {
@@ -35,5 +35,5 @@ const config = {
 
 // on error a fallback kiwi may be used. This kiwi must have no time limits at all
 const fallback = true;
-const fallbackUrl = "http://kiwi-vih.aprs.fi:8073/?keys=x";
+const fallbackUrl = "http://kiwi-vih.aprs.fi:8073/";
 const fallbackReloadMinutes = 30;
