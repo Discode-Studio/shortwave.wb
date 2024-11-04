f06e9bdc082b895ed5ea9630c0532872f14950cd
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-11-04 18:46:24 +0100

    Update rtmp2.py

--- rtmp2.py
+++ rtmp2.py
@@ -5,7 +5,7 @@ command = [
     'ffmpeg',
     '-re',                      # Lire à la vitesse réelle
     '-stream_loop', '-1',        # Répéter la vidéo en boucle infinie
-    '-i', 'Projet_11-03(1)_SD 360p_LOW_FR24.mp4',  # Vidéo source
+    '-i', 'Projet_11-04_HD 720p_LOW_FR24.mp4',  # Vidéo source
     '-c:v', 'libx264',           # Codeur vidéo H.264
     '-preset', 'veryfast',       # Réduction de la latence
     '-b:v', '3000k',             # Débit vidéo de 3000 kbps
